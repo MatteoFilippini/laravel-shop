783c4d9aceb1d582bbc65bbdb7ed70c10ceedd14
niculas1989 <nicolas.maranzano@libero.it>
2022-03-24 16:55:39 +0100

    added button on show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>
-        <figure>
-            <img src="{{ $product->image }}" alt="">
-        </figure>
-        <h2>{{ $product->name }}</h2>
-        <p>{{ $product->description }}</p>
-        <div>{{ $product->price }}</div>
-        <a href="{{ route('products.index') }}">Home</a>
-    </div>
+<div>
+    <figure>
+        <img src="{{ $product->image }}" alt="">
+    </figure>
+    <h2>{{ $product->name }}</h2>
+    <p>{{ $product->description }}</p>
+    <div>{{ $product->price }}</div>
+    <a href="{{ route('products.index') }}">Home</a>
+    <!-- AGGIUNTO collegamento per modificare il prodotto -->
+    <a href="{{ route('products.edit', $product->id) }}" role="button">
+        MODIFICA
+    </a>
+</div>
 @endsection
