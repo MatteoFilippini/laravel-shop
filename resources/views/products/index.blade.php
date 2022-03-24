e6799d09c26aa97deb27e2e462dd3b7d12e08970
niculas1989 <nicolas.maranzano@libero.it>
2022-03-24 16:42:53 +0100

    issues done - added edit + update info

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,6 +11,14 @@
         <p>{{ $product->description }}</p>
         <div>{{ $product->price }}</div>
         <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
+        <div>
+            <!-- AGGIUNTO collegamento per modificare il prodotto -->
+            <a href="{{ route('products.edit', $product->id) }}" role="button">
+                MODIFICA
+            </a>
+        </div>
+
+
     </li>
     @endforeach
 </ul>
