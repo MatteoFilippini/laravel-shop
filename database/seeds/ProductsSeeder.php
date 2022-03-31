badfaa0507af467943ecc3b94fbe5ea7ba0e58f7
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-31 16:41:30 +0200

    finish

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\Models\Product;
 use Faker\Generator as Faker;
+use App\Models\Brand;
+use Illuminate\Support\Arr;
 
 class ProductsSeeder extends Seeder
 {
@@ -13,12 +15,15 @@ class ProductsSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $brand_ids = Brand::pluck('id')->toArray();
+
         for ($i = 0; $i < 15; $i++) {
             $new_product = new Product();
             $new_product->name = ucfirst($faker->word());
             $new_product->description = $faker->paragraph();
             $new_product->price = $faker->randomFloat(2, 1, 999);
             $new_product->image = $faker->imageUrl(200, 200);
+            $new_product->brand_id = Arr::random($brand_ids);
             $new_product->save();
         }
     }
